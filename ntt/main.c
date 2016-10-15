ec0ac10c5be9f283fcf41e455249fa908b20fe4a
安志钢 <AnZhg@AnZhg-MacBook-Pro.local>
2016-10-15 18:42:48 -0400

    Initial Commit

---
+++ ntt/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ntt
+//
+//  Created by 安志钢 on 16-10-15.
+//  Copyright (c) 2016年 安志钢. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
