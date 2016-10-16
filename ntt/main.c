03f600833a464e56263ff7a0f93c3f88e984b51f
安志钢 <AnZhg@AnZhg-MacBook-Pro.local>
2016-10-16 17:42:24 -0400

    Working copy.

--- ntt/main.c
+++ ntt/main.c
@@ -7,12 +7,56 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include "ntt.h"
+
+#include <limits.h>
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    // Driver program for NTT and INTT.
+    int i;
+    
+    unsigned int *io = malloc(16 * sizeof(unsigned int));
+    io[0] = 12u;
+    io[1] = 34u;
+    io[2] = 56u;
+    io[3] = 78u;
+    io[4] = 90u;
+    io[5] = 22u;
+    io[6] = 66u;
+    io[7] = 99u;
+    io[8] = 12u;
+    io[9] = 34u;
+    io[10] = 56u;
+    io[11] = 78u;
+    io[12] = 90u;
+    io[13] = 22u;
+    io[14] = 66u;
+    io[15] = 99u;
+    
+    printf("Original:\n");
+    for (i = 0; i < 16; ++i) {
+        printf("#%02i: %i\n", i + 1, io[i]);
+    }
+    printf("\n");
+    
+    ntt(&io);
+    
+    printf("After NTT:\n");
+    for (i = 0; i < 16; ++i) {
+        printf("#%02i: %i\n", i + 1, io[i]);
+    }
+    printf("\n");
+    
+    intt(&io);
+    
+    printf("After INTT:\n");
+    for (i = 0; i < 16; ++i) {
+        printf("#%02i: %i\n", i + 1, io[i]);
+    }
+    printf("\n");
+    
     return 0;
 }
 
---
+++ ntt/ntt.h
@@ -0,0 +1,22 @@
+//
+//  ntt.h
+//  fft
+//
+//  Created by 安志钢 on 16-10-14.
+//  Copyright (c) 2016年 安志钢. All rights reserved.
+//
+// Number theoretic transform and inverse.
+
+#ifndef fft_ntt_h
+#define fft_ntt_h
+
+/*
+ In place 16-point Number Theoretic Trabsform and Inverse Number Theoretic Trabsform.
+ io: input and output. Pass by reference.
+ Every element has to be 0 ~ 192 inclusive. (unsigned int)
+ Larger than 192 would cause loss in information. (Due to mod 193)
+ */
+void ntt(unsigned int *io[16]);
+void intt(unsigned int *io[16]);
+
+#endif
